f279c21f36f65fde8f2cbc6afc31ad3506cf3f8a
chenlian98 <761171478@qq.com>
2020-02-14 15:14:16 +0800

    add-json-server

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import { Button, Select } from "element-ui";
+
+Vue.component(Button, Select);
 
 Vue.config.productionTip = false;
 
